0babebaaf0807f0247f06c0ab25bdc25fd8b6009
lqtm <austinwalela@gmail.com>
2020-02-07 11:05:19 +0300

    Added post functionality to create campaigns

--- save-the-animals/src/components/CreateCampaign.js
+++ save-the-animals/src/components/CreateCampaign.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {
   FormControl,
   Button,
@@ -30,62 +31,127 @@ const Heading = styled.h1`
 `
 
 export default function CreateCampaign() {
+  const initialValues = {
+    title: '',
+    description: '',
+    urgency_level: 1,
+    funding_goal: 100,
+    deadline: '',
+    specie_id: 1,
+    location: '',
+  }
+  const [formValues, setFormValues] = useState(initialValues)
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(formValues)
+    axios
+      .post('https://save-all-the-animals.herokuapp.com/api/campaigns', {
+        title: formValues.title,
+        photo: '',
+        description: formValues.description,
+        urgency_level: formValues.urgency_level,
+        funding_goal: formValues.funding_goal,
+        deadline: formValues.deadline,
+        specie_id: formValues.specie_id,
+        location: formValues.location,
+      })
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+    setFormValues(initialValues)
+  }
+  const handleChange = e => {
+    if (e.target.type === 'select-one') {
+      setFormValues({
+        ...formValues,
+        [e.target.name]: e.target.options[
+          e.target.selectedIndex
+        ].index.toString(),
+      })
+      console.log(e.target.options[e.target.selectedIndex].index.toString())
+    } else {
+      setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
+  }
   return (
     <React.Fragment>
       <NavBar page='signout' />
       <CampaignContainer>
         <Heading>Create a Campaign</Heading>
-        <FormControl>
-          <Stack w='30vw' m='0 auto'>
-            <Input placeholder='Title' size='md' />
-            <Textarea
-              name='description'
-              placeholder='Describe your campaign in a few words'
-            />
-            <Input placeholder='Location' size='md' />
-            <FormHelperText>Urgency Between 1-5</FormHelperText>
-            <NumberInput
-              defaultValue={1}
-              min={1}
-              max={5}
-              clampValueOnBlur={false}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            <FormHelperText>Funding Goal</FormHelperText>
-            <NumberInput
-              defaultValue={100}
-              step={100}
-              min={100}
-              max={50000}
-              clampValueOnBlur={false}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            <FormHelperText>Species ID</FormHelperText>
-            <NumberInput
-              defaultValue={1}
-              min={1}
-              max={12}
-              clampValueOnBlur={false}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            <Input placeholder='Deadline: DD/MM/YY' size='md' />
-            <Button mt={4} variantColor='green' type='submit'>
-              Submit
-            </Button>
-          </Stack>
-        </FormControl>
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <Stack w='30vw' m='0 auto'>
+              <Input
+                placeholder='Title'
+                value={initialValues.title}
+                onChange={handleChange}
+                size='md'
+              />
+              <Textarea
+                name='description'
+                placeholder='Describe your campaign in a few words'
+                value={initialValues.description}
+                onChange={handleChange}
+              />
+              <Input placeholder='Location' size='md' />
+              <FormHelperText>Urgency Between 1-5</FormHelperText>
+              <NumberInput
+                min={1}
+                max={5}
+                defaultValue={initialValues.urgency_level}
+                value={initialValues.urgency_level}
+                onChange={handleChange}
+                clampValueOnBlur={false}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <FormHelperText>Funding Goal</FormHelperText>
+              <NumberInput
+                defaultValue={100}
+                step={100}
+                min={100}
+                max={50000}
+                value={initialValues.funding_goal}
+                onChange={handleChange}
+                clampValueOnBlur={false}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <FormHelperText>Species ID</FormHelperText>
+              <NumberInput
+                defaultValue={1}
+                min={1}
+                max={12}
+                value={initialValues.specie_id}
+                onChange={handleChange}
+                clampValueOnBlur={false}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <Input
+                placeholder='Deadline: DD/MM/YY'
+                value={initialValues.deadline}
+                onChange={handleChange}
+                size='md'
+              />
+              <Button mt={4} variantColor='green' type='submit'>
+                Submit
+              </Button>
+            </Stack>
+          </FormControl>
+        </form>
       </CampaignContainer>
     </React.Fragment>
   )
